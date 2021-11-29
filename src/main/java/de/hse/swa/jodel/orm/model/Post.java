34ae756c0f40a16f6bf6521eda3f19478bdf994d
DDimov98 <dimitar.dimov98@web.de>
2021-11-29 23:05:28 +0100

    Update Model Post

--- src/main/java/de/hse/swa/jodel/orm/model/Post.java
+++ src/main/java/de/hse/swa/jodel/orm/model/Post.java
@@ -39,7 +39,32 @@ public class Post  {
     private Long authorId;
 
     @Column(name = "pdat")
-    private Date postedat;
+    private Date pdat;
 
     public Post() { }
+
+    public Post(String text, Long userId) {
+        this.text = text;
+        this.authorId = userId;
+        this.pdat = new Date();
+    }
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id =id;}
+
+    public String getText() {return text;}
+
+    public void setText(String text) {this.text = text;}
+
+    public Date getPdat() {return pdat;}
+
+    public void setPdat(Date pdat) {this.pdat = pdat;}
+
+    public Long getAuthorId() {return  this.authorId;}
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
 }
+
