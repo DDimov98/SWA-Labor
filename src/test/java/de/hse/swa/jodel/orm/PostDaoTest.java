8f79357d33c686b80bc73e5887d16761e8ae8504
DDimov98 <dimitar.dimov98@web.de>
2021-11-30 21:18:15 +0100

    Create PostDaoTest
    -createUser Function

---
+++ src/test/java/de/hse/swa/jodel/orm/PostDaoTest.java
@@ -0,0 +1,37 @@
+package de.hse.swa.jodel.orm;
+
+//imports//
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import de.hse.swa.jodel.orm.model.Post;
+import de.hse.swa.jodel.orm.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.hse.swa.jodel.orm.dao.UserDao;
+import de.hse.swa.jodel.orm.dao.PostDao;
+import io.quarkus.test.junit.QuarkusTest;
+public class PostDaoTest {
+
+        @Inject
+        UserDao userDao;
+
+        @Inject
+        PostDao postDao;
+
+        private User createUser(String name) {
+            User user = new User();
+            user.setUsername(name);
+            user.setPassword("password");
+            userDao.save(user);
+            return user;
+        }
+
+}
