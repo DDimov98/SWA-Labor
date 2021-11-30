54d707d764b20facd4a9421f556ff9afd55ede6e
Mo <75176339+xMungix@users.noreply.github.com>
2021-11-30 12:58:49 +0100

    PostDao update
    Functions getPost

--- src/main/java/de/hse/swa/jodel/orm/dao/PostDao.java
+++ src/main/java/de/hse/swa/jodel/orm/dao/PostDao.java
@@ -9,9 +9,14 @@
  *========================================================================*/
 package de.hse.swa.jodel.orm.dao;
 
+import de.hse.swa.jodel.orm.model.Post;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @ApplicationScoped
 public class PostDao {
@@ -19,5 +24,31 @@ public class PostDao {
     @Inject
     EntityManager em;
 
+
+    public List<Post> getAllPosts() {
+        //TypedQuery<Post> query = em.createQuery("select u from Post u");
+        //List<Post> results = query.getResultList();
+        //return results;
+
+        Query q = em.createQuery("select u from Post u ORDER BY postedat DESC");
+        List<Post> posts = q.getResultList();
+        return posts;
+    }
+
+
+    public List<Post> getPosts(Double lat, Double lon) {
+        TypedQuery<Post> query = em.createQuery("SELECT u FROM Post u WHERE u.longitude > '" + (lon - 0.14) + "'  AND " +
+                " u.longitude < '" + (lon + 0.14) + "' AND " +
+                " u.latitude > '"+ (lat - 0.14) + "' AND " +
+                " u.latitude < '" + (lat + 0.14) + "'", Post.class);
+        List<Post> results = query.getResultList();
+        return results;
+
+    }
+
+
+    public Post getPost(Long id) {
+        return em.find(Post.class, id);
+    }
 
 }
