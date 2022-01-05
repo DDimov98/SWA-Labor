97b9b53def55444f85b06cfc1de7c407c1f060fa
Mo <75176339+xMungix@users.noreply.github.com>
2022-01-05 19:49:26 +0100

    Change Design of Post and Comment

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -3,31 +3,85 @@ import { withStyles } from '@material-ui/core/styles';
 
 import Comments from './Comments';
 import PostForm from './PostForm';
+import FormControl from "@material-ui/core/FormControl";
 
 const styles = theme => ({
 	center: {
-		display: 'block',
-		border: '5px solid green',
-		padding: '10px',
-	}
+		width: "96%",
+		padding: "2%",
+		boxSizing: "borderBox",
+		borderRadius: "3px 15px 5px 30px",
+		background: "#5DCD",
+		marginTop: "2%",
+	},
+
+	postForm: {
+		marginLeft: "0%",
+		width: "80%",
+		padding: "2%",
+		boxSizing: "borderBox",
+		borderRadius: "10px 10px 0px 10px",
+		background: "#5858",
+		marginTop: "2%",
+		fontFamily: "monospace",
+		color: "black",
+	},
+
+	textForm: {
+		marginLeft: "10%",
+		width: "80%",
+		padding: "2%",
+		boxSizing: "borderBox",
+		borderRadius: "0px 10px 10px 10px",
+		background: "#A4A4A4",
+		marginTop: "0%",
+		fontFamily: "serif",
+		color: "grey",
+	},
 });
 
 class Post extends React.Component {
 
 	constructor(props) {
 		super(props);
-	    this.state = {	  
-            data: [],
-		};		
+	    this.state = {
+			currentUser: this.props.currentUser,
+            post: this.props.post,
+			author: [],
+			comments: [],
+		};
 	}
-	
-	render() {
+
+	componentDidMount()
+	{
+		fetch( "http://localhost:8080/Users/getUser?userId=" + this.props.author)
+			.then( function(response) { return response.json() } )
+			.then( data => this.setState({author: data }));
+		this.setState({loading: false});
+	}
+
+
+render() {
 		const { classes } = this.props;
+	console.log( "Benutzer ist in Post:" + this.state.currentUser.username)
 		return (
 		<div className={classes.center}>
-			<h3>This component renders a single post</h3>
-			<Comments></Comments>
-            <PostForm></PostForm>
+
+            <font >
+
+				<div className={classes.postForm}>
+					{this.state.author.username} schieb am {this.state.post.postedat.slice(0,10).replace("-",".").replace("-",".") } um {this.state.post.postedat.slice(11,16)}
+
+				</div>
+
+				<div className={classes.textForm}>
+					{this.state.post.text}
+				</div>
+
+			</font>
+
+			<Comments currentUser={this.state.currentUser} comments={this.state.comments} post={this.state.post}></Comments>
+            <PostForm post = {this.state.post} currentUser={this.state.currentUser}></PostForm>
 	    </div>
 		);
 	}
