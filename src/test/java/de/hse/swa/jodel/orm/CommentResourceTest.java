b9bf0652177bbd6abd972ff0fa6278b41aad9479
DDimov98 <dimitar.dimov98@web.de>
2022-01-05 11:14:14 +0100

    Update CommentResourceTest

--- src/test/java/de/hse/swa/jodel/orm/CommentResourceTest.java
+++ src/test/java/de/hse/swa/jodel/orm/CommentResourceTest.java
@@ -23,11 +23,12 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.QuarkusTest;
 
-
+import de.hse.swa.jodel.orm.model.Voting;
 import de.hse.swa.jodel.orm.model.Comment;
 import de.hse.swa.jodel.orm.model.Post;
 import de.hse.swa.jodel.orm.model.User;
 
+import de.hse.swa.jodel.orm.dao.VotingDao;
 import de.hse.swa.jodel.orm.dao.CommentDao;
 import de.hse.swa.jodel.orm.dao.PostDao;
 import de.hse.swa.jodel.orm.dao.UserDao;
@@ -44,5 +45,32 @@ import javax.inject.Inject;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+@QuarkusTest
 public class CommentResourceTest {
+
+    //Post 1
+    @Test
+    @Order(10)
+    public void GetComments()
+    {
+        given()
+
+                .when().get("/Comments/getComments?postId=1")
+                .then()
+                .body(is("[{\"authorId\":1,\"id\":1,\"latitude\":2345.0,\"longitude\":245.0,\"postId\":1,\"postedat\":\"2022-01-05T00:00:00Z[UTC]\",\"text\":\"CommentNumber1\"}]"));
+    }
+
+    //Post 2
+    @Test
+    @Order(20)
+    public void GetCommentsForPost2()
+    {
+        given()
+
+                .when().get("/Comments/getComments?postId=2")
+                .then()
+
+                .body(is("[{\"authorId\":2,\"id\":2,\"latitude\":2545.0,\"longitude\":87.0,\"postId\":2,\"postedat\":\"2022-01-05T00:00:00Z[UTC]\",\"text\":\"FirstTest\"}]"));
+    }
+
 }
