dd87eead23d2721e158adb00c562b7e266b8559a
Mo <75176339+xMungix@users.noreply.github.com>
2022-01-07 16:44:21 +0100

    Update CommentForm.jsx

--- client/src/CommentForm.jsx
+++ client/src/CommentForm.jsx
@@ -1,33 +1,64 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles';
-
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
-	center: {
-		display: 'flex', 
-		justifyContent: 'center',
-		alignItems: 'center',
-		minHeight: '100vh',
+
+
+
+	kommentar: {
+		marginLeft: "10%",
+		width: "85%",
+		padding: "2%",
+		boxSizing: "borderBox",
+		borderRadius: "0px 10px 10px 0px",
+		background: "#A4A4A4",
+		marginTop: "0%",
+		fontFamily: "cursive",
+		color: "black",
+	},
+
+	kommentar2: {
+		width: "94%",
+		padding: "3%",
+		boxSizing: "borderBox",
+		borderRadius: "10px 10px 10px 0px",
+		background: "#A4A4A4",
+		marginTop: "1%",
+	},
+	button:{
+		align: "left",
+		marginTop: '100px',
 	}
+
 });
 
 class CommentForm extends React.Component {
 
 	constructor(props) {
 		super(props);
-	    this.state = {	  
-            data: [],
-		};		
+	    this.state = {
+			currentUser: this.props.currentUser,
+			comment: this.props.comment,
+		};
 	}
-	
+
+
+	upvote(){}
+	downvote(){}
+
 	render() {
 		const { classes } = this.props;
 		return (
-		<div className={classes.center}>
-			<h1>This component renders the input form for a comment</h1>
-	    </div>
+		<div className={classes.kommentar}>
+			{this.state.comment.text}
+		</div>
+
+
+
 		);
 	}
+
 }
 
 export default withStyles(styles)(CommentForm);
