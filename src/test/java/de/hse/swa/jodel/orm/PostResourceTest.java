096f7c54dc0c0d8f5cefacf488f40b2dbe4acc3f
DDimov98 <dimitar.dimov98@web.de>
2022-01-10 16:18:46 +0100

    Update PostResourceTest.java

--- src/test/java/de/hse/swa/jodel/orm/PostResourceTest.java
+++ src/test/java/de/hse/swa/jodel/orm/PostResourceTest.java
@@ -27,11 +27,7 @@ public class PostResourceTest
                 .when().get("/Post/getAllPosts")
                 .then()
 
-                .body(is("[{\"authorId\":1,\"id\":1,\"latitude\":24.0,\"longitude\":25345.0,\"postedat\":" +
-                        "\"2021-12-09T00:00:00Z[UTC]\",\"text\":\"Das ist ein Post\"},{\"authorId\":2,\"id\":2,\"latitude\":32265.0,\"longitude\":2324.0,\"postedat\":" +
-                        "\"2021-12-09T00:00:00Z[UTC]\",\"text\":\"Und das ist der zweite Post\"},{\"authorId\":1,\"id\":3,\"latitude\":24.0,\"longitude\":25345.0,\"postedat\":" +
-                        "\"2021-12-09T00:00:00Z[UTC]\",\"text\":\"Ich habe keine lust mehr\"},{\"authorId\":2,\"id\":4,\"latitude\":32265.0,\"longitude\":2324.0,\"postedat\":" +
-                        "\"2021-12-09T00:00:00Z[UTC]\",\"text\":\"Das ist eaber schönes wetter\"}]"));
+                .body(is("[{\"authorId\":1,\"id\":1,\"latitude\":42.0,\"longitude\":25345.0,\"postedat\":\"2022-01-01T00:00:00Z[UTC]\",\"text\":\"First Post\"},{\"authorId\":2,\"id\":2,\"latitude\":42042.0,\"longitude\":6969.0,\"postedat\":\"2022-01-01T00:00:00Z[UTC]\",\"text\":\"Second Post\"},{\"authorId\":1,\"id\":3,\"latitude\":42.0,\"longitude\":25345.0,\"postedat\":\"2022-01-01T00:00:00Z[UTC]\",\"text\":\"Third Post\"}]"));
 
     }
 
@@ -42,9 +38,9 @@ public class PostResourceTest
     public void GetPosts()
     {
         given()
-                .when().get("/Post/getPosts?lat=24&lon=25345")
+                .when().get("/Post/getPosts?lat=42&lon=25345")
                 .then()
-                .body(is("[{\"authorId\":1,\"id\":1,\"latitude\":24.0,\"longitude\":25345.0,\"postedat\":\"2020-09-09T00:00:00Z[UTC]\",\"text\":\"Das ist ein Post\"},{\"authorId\":1,\"id\":3,\"latitude\":24.0,\"longitude\":25345.0,\"postedat\":\"2020-09-09T00:00:00Z[UTC]\",\"text\":\"Ich habe keine lust mehr\"}]"));
+                .body(is("[{\"authorId\":1,\"id\":1,\"latitude\":42.0,\"longitude\":25345.0,\"postedat\":\"2022-01-01T00:00:00Z[UTC]\",\"text\":\"First Post\"},{\"authorId\":1,\"id\":3,\"latitude\":42.0,\"longitude\":25345.0,\"postedat\":\"2022-01-01T00:00:00Z[UTC]\",\"text\":\"Third Post\"}]"));
     }
 
    /* @Test
